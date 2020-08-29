84a81769371ca40aa25109b74d4cd4952486d7e3
henry <henry.odiete@opin.ca>
2020-08-29 14:24:23 -0400

    Updates

---
+++ precache-manifest.1e51ed278922a9a2d88b33e3090bafcf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReadsApp/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReadsApp/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReadsApp/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "42f333e3d3e9fb5f9d28",
+    "url": "/MyReadsApp/static/js/runtime~main.42f333e3.js"
+  },
+  {
+    "revision": "ca02372d665610e2680b",
+    "url": "/MyReadsApp/static/js/main.ca02372d.chunk.js"
+  },
+  {
+    "revision": "e24fc0232193f157ff7c",
+    "url": "/MyReadsApp/static/js/1.e24fc023.chunk.js"
+  },
+  {
+    "revision": "ca02372d665610e2680b",
+    "url": "/MyReadsApp/static/css/main.a904cce2.chunk.css"
+  },
+  {
+    "revision": "d74973da663a4c6162e04cfdffeb9099",
+    "url": "/MyReadsApp/index.html"
+  }
+];
